0e62d8b658d1df63d853ee5259ebd05f3486796b
Nikita <nikzakharenkov@gmail.com>
2023-05-28 17:45:11 +0300

    feat: improved recycler view and added button for save image and open in browser

--- app/src/main/java/com/example/vrgsofttest/model/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/vrgsofttest/model/RecyclerViewAdapter.kt
@@ -1,7 +1,5 @@
 package com.example.vrgsofttest.model
 
-import android.graphics.BitmapFactory
-import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,29 +9,24 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.vrgsofttest.R
 import com.example.vrgsofttest.data.RedditPost
 import com.squareup.picasso.Picasso
-import java.io.InputStream
-import java.net.URL
 import java.util.concurrent.TimeUnit
 
 class RecyclerViewAdapter(private val posts:List<RedditPost>):
     RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        // inflates the card_view_design view
-        // that is used to hold list item
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.card_view_design, parent, false)
 
         return ViewHolder(view)
     }
 
-    // binds the list items to a view
+
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
         val ItemsViewModel = posts[position]
 
-        // sets the image to the imageview from our itemHolder class
-        //holder.imageView.
-        if(ItemsViewModel.is_video==true){
+
+        if(ItemsViewModel.is_video){
             holder.thumbnailImageView.setImageResource(R.drawable.video_support)
         }else{
             Picasso.get().load(ItemsViewModel.thumbnail).into(holder.thumbnailImageView)
@@ -41,19 +34,17 @@ class RecyclerViewAdapter(private val posts:List<RedditPost>):
 
 
 
-        // sets the text to the textview from our itemHolder class
         holder.titleTextView.text = ItemsViewModel.title
         holder.authorTextView.text ="Author:"+ ItemsViewModel.author
         holder.timeTextView.text="Created"+getTimeAgo(ItemsViewModel.created_utc)+"hourse ago"
         holder.commentsTextView.text="Comments:"+ItemsViewModel.num_comments.toString()
     }
 
-    // return the number of the items in the list
+
     override fun getItemCount(): Int {
         return posts.size
     }
 
-    // Holds the views for adding it to image and text
     class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
         val thumbnailImageView: ImageView = itemView.findViewById(R.id.thumbnailImageView)
         val titleTextView: TextView = itemView.findViewById(R.id.titleTextView)
